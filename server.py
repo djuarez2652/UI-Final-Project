8066b93aff1dae2d38a859e323df75af61e6d03a
djuarez2652 <djuarez0440@gmail.com>
2026-04-20 00:37:45 -0400

    feat: impl basic home; impl basic nav

--- server.py
+++ server.py
@@ -8,5 +8,20 @@ def home():
     return render_template("welcome.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
